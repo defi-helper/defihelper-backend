c72f32c64d1e7233f373dddb1eb7e39fe75837fb
Andrey Frimuchkov <afrimuchkov@yandex.ru>
2021-08-02 10:12:18 +0300

    Handle TG errors

--- src/services/Telegram/templates/index.ts
+++ src/services/Telegram/templates/index.ts
@@ -2,4 +2,5 @@ import fs from 'fs';
 
 export const Templates = {
   eventTemplate: fs.promises.readFile(`${__dirname}/Event.mustache`, 'utf8'),
+  welcomeTemplate: fs.promises.readFile(`${__dirname}/Welcome.mustache`, 'utf8'),
 };
