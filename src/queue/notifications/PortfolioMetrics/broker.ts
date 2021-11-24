4350523fc2b9c8d30b5d2edcca17b9a5dc7b6b17
Luka Nikitin <lukentuichennel@gmail.com>
2021-11-24 15:23:35 +0300

    fix

--- src/queue/notifications/PortfolioMetrics/broker.ts
+++ src/queue/notifications/PortfolioMetrics/broker.ts
@@ -1,21 +1,21 @@
 import { Process } from '@models/Queue/Entity';
 import container from '@container';
 import { userContactTableName } from '@models/Notification/Entity';
-import { tableName as userTableName } from '@models/User/Entity';
+import { Role, tableName as userTableName } from '@models/User/Entity';
 import dayjs from 'dayjs';
 
 export default async (process: Process) => {
   const users = await container.model
     .userTable()
-    .select(`${userTableName}.id as uId`)
-    .innerJoin(userContactTableName, `${userContactTableName}.user`, '=', `${userTableName}.id`)
-    .groupBy('uId');
+    .distinct(`${userTableName}.*`)
+    .innerJoin(userContactTableName, `${userTableName}.id`, `${userContactTableName}.user`)
+    .whereNot(`${userTableName}.role`, Role.Candidate);
 
   const lag = 86400 / users.length; // seconds in day
   await users.reduce<Promise<dayjs.Dayjs>>(async (prev, user) => {
     const startAt = await prev;
     await container.model.queueService().push('notificationPortfolioMetricsNotify', {
-      userId: user.uId,
+      userId: user.id,
     });
     return startAt.clone().add(lag, 'seconds');
   }, Promise.resolve(dayjs()));
--- src/queue/notifications/PortfolioMetrics/sender.ts
+++ src/queue/notifications/PortfolioMetrics/sender.ts
@@ -1,6 +1,6 @@
 import { Process } from '@models/Queue/Entity';
 import container from '@container';
-import { ContactBroker } from '@models/Notification/Entity';
+import { ContactBroker, ContactStatus } from '@models/Notification/Entity';
 import { DataLoaderContainer } from '@api/dataLoader/container';
 
 export default async (process: Process) => {
@@ -11,32 +11,35 @@ export default async (process: Process) => {
     throw new Error('User not found');
   }
 
+  const dataLoader = new DataLoaderContainer({});
   const contacts = await container.model.userContactTable().where({
     user: user.id,
     broker: ContactBroker.Telegram,
+    status: ContactStatus.Active,
   });
 
-  const dataLoader = new DataLoaderContainer({});
-  for (const contact of contacts) {
-    const chatId = contact.params?.chatId;
+  await Promise.all(
+    contacts
+      .filter((v) => v.params?.chatId)
+      .map(async (contact) => {
+        const chatId = contact.params?.chatId;
 
-    if (!chatId) {
-      continue;
-    }
+        const [totalStackedUSD, totalEarnedUSD] = await Promise.all([
+          dataLoader.userMetric({ metric: 'stakingUSD' }).load(user.id),
+          dataLoader.userMetric({ metric: 'earnedUSD' }).load(user.id),
+        ]);
 
-    const totalStackedUSD = await dataLoader.userMetric({ metric: 'stakingUSD' }).load(user.id);
-    const totalEarnedUSD = await dataLoader.userMetric({ metric: 'stakingUSD' }).load(user.id);
-
-    await container.model.queueService().push('sendTelegram', {
-      chatId,
-      locale: user.locale,
-      params: {
-        totalStackedUSD,
-        totalEarnedUSD,
-      },
-      template: 'portfolioMetrics',
-    });
-  }
+        return container.model.queueService().push('sendTelegram', {
+          chatId,
+          locale: user.locale,
+          params: {
+            totalStackedUSD,
+            totalEarnedUSD,
+          },
+          template: 'portfolioMetrics',
+        });
+      }),
+  );
 
   return process.done();
 };
