984f41acab48c59e71d4f0bb099e568725807b31
Luka Nikitin <lukentuichennel@gmail.com>
2021-11-25 16:33:52 +0300

    fix number format

--- src/queue/notifications/PortfolioMetrics/sender.ts
+++ src/queue/notifications/PortfolioMetrics/sender.ts
@@ -32,8 +32,8 @@ export default async (process: Process) => {
         chatId,
         locale: user.locale,
         params: {
-          totalStackedUSD,
-          totalEarnedUSD,
+          totalStackedUSD: totalStackedUSD.toFixed(2),
+          totalEarnedUSD: totalEarnedUSD.toFixed(2),
         },
         template: 'portfolioMetrics',
       });
