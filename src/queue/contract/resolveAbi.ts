81ed97290a56eee831c1a7b6e6934e2d81d94ac1
Luka Nikitin <lukentuichennel@gmail.com>
2021-11-30 20:28:54 +0300

    fix

--- src/queue/contract/resolveAbi.ts
+++ src/queue/contract/resolveAbi.ts
@@ -1,31 +1,25 @@
 import { Process } from '@models/Queue/Entity';
 import container from '@container';
 import dayjs from 'dayjs';
-import { Blockchain } from '@models/types';
 import { MetadataType } from '@models/Protocol/Entity';
 
 export interface Params {
   id: string;
-  blockchain: Blockchain;
 }
 
 export default async (process: Process) => {
-  const { id, blockchain } = process.task.params as Params;
-
-  if (blockchain === 'waves') {
-    throw new Error('Unsupported blockchain');
-  }
+  const { id } = process.task.params as Params;
 
   const contractService = container.model.contractService();
   const metadataService = container.model.metadataService();
 
   const contract = await contractService.contractTable().where({ id }).first();
-  if (!contract) {
-    throw new Error(`Contract "${id}" not found`);
+  if (!contract || contract.blockchain === 'waves') {
+    throw new Error(`Contract "${id}" not found or incompatible`);
   }
 
   try {
-    const network = container.blockchain[blockchain].byNetwork(contract.network);
+    const network = container.blockchain[contract.blockchain].byNetwork(contract.network);
     const abi = await network.getContractAbi(contract.address);
 
     await metadataService.createOrUpdate(contract, MetadataType.EthereumContractAbi, abi);
