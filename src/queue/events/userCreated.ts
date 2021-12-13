85ad0eedfd99ca679eb284006c3631f18109ede9
artur <amamedbekov@tokenomica.com>
2021-12-13 17:47:38 +0300

    free notification for new users

--- src/queue/events/userCreated.ts
+++ src/queue/events/userCreated.ts
@@ -1,5 +1,6 @@
 import container from '@container';
 import { Process } from '@models/Queue/Entity';
+import { ProductCode } from '@models/Store/Entity';
 import { UserNotificationType } from '@models/UserNotification/Entity';
 
 export interface Params {
@@ -11,6 +12,32 @@ export default async (process: Process) => {
   const user = await container.model.userTable().where('id', id).first();
   if (!user) throw new Error('User not found');
 
+  // Free notifications
+  const notificationProduct = await container.model
+    .storeProductTable()
+    .where({
+      code: ProductCode.Notification,
+      amount: 100,
+    })
+    .first();
+  if (notificationProduct) {
+    const firstWallet = await container.model.walletTable().where('user', user.id).first();
+    if (firstWallet) {
+      await container.model
+        .storeService()
+        .purchase(
+          notificationProduct,
+          firstWallet.blockchain,
+          firstWallet.network,
+          firstWallet.address,
+          notificationProduct.amount,
+          '',
+          new Date(),
+        );
+    }
+  }
+
+  // Enable all notifications
   await Promise.all(
     Object.values(UserNotificationType).map((t) =>
       container.model.userNotificationService().enable(user, t as UserNotificationType),
