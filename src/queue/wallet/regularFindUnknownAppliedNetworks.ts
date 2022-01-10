ab8593fc7e5f29c40f3e3c1600b8c7ad5e2d76c3
Luka Nikitin <lukentuichennel@gmail.com>
2022-01-10 17:52:20 +0300

    fix

---
+++ src/queue/wallet/regularFindUnknownAppliedNetworks.ts
@@ -0,0 +1,22 @@
+import { Process } from '@models/Queue/Entity';
+import container from '@container';
+import dayjs from 'dayjs';
+
+export default async (process: Process) => {
+  const wallets = await container.model.walletTable();
+
+  const lag = 86400 / wallets.length; // seconds in day
+  await wallets.reduce<Promise<dayjs.Dayjs>>(async (prev, wallet) => {
+    const startAt = await prev;
+    await container.model.queueService().push(
+      'findWalletAppliedNetworks',
+      {
+        walletId: wallet.id,
+      },
+      { startAt: startAt.toDate() },
+    );
+    return startAt.clone().add(lag, 'seconds');
+  }, Promise.resolve(dayjs()));
+
+  return process.done();
+};
