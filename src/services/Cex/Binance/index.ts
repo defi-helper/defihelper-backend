8e587d820b100750ad406b09c9588e15ceeb8139
Luka Nikitin <lukentuichennel@gmail.com>
2022-01-29 12:20:27 +0400

    fix

--- src/services/Cex/Binance/index.ts
+++ src/services/Cex/Binance/index.ts
@@ -1,20 +1,19 @@
 import { ICexService } from '@services/Cex';
-import Binance from 'node-binance-api';
+import Binance from 'binance-api-node';
 
 export default class implements ICexService {
   validateAccount = async (account: { apiKey: string; apiSecret: string }): Promise<boolean> => {
-    const binance = new Binance().options({
-      APIKEY: account.apiKey,
-      APISECRET: account.apiSecret,
-      useServerTime: true,
-    } as any); // fixme as any
+    const binance = Binance({
+      apiKey: account.apiKey,
+      apiSecret: account.apiSecret,
+    });
 
     try {
-      // todo check error code
-      await binance.balance();
-      return true;
+      await binance.accountInfo();
     } catch {
       return false;
     }
+
+    return true;
   };
 }
