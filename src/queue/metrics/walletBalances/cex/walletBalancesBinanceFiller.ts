130772c3cdd384b8b6e1d6983f883e7db91de6c4
Luka Nikitin <lukentuichennel@gmail.com>
2022-02-02 16:35:47 +0300

    fix

--- src/queue/metrics/walletBalances/cex/walletBalancesBinanceFiller.ts
+++ src/queue/metrics/walletBalances/cex/walletBalancesBinanceFiller.ts
@@ -2,6 +2,7 @@ import container from '@container';
 import { Process } from '@models/Queue/Entity';
 import Binance, { AssetBalance } from 'binance-api-node';
 import { WalletSuspenseReason } from '@models/Wallet/Entity';
+import BN from 'bignumber.js';
 
 interface Params {
   id: string;
@@ -24,19 +25,41 @@ export default async (process: Process) => {
   });
 
   let spotAssetsList: AssetBalance[];
+  let prices: { [key: string]: string };
   try {
     spotAssetsList = (await binance.accountInfo()).balances;
+    prices = await binance.prices();
   } catch (e) {
     await container.model
       .walletService()
       .suspense(exchangeWallet.id, WalletSuspenseReason.CexUnableToAuthorize);
 
     console.warn(e);
-
     return process.error(e).info('Wallet freezed');
   }
 
-  console.warn(spotAssetsList);
+  const resolveTokenPrice = (symbol: string) => {
+    return ['USDT', 'BUSD'].map((bridge) => prices[symbol + bridge]).find((v) => v);
+  };
+  const assetsOnBalance = spotAssetsList
+    .filter((v) => !new BN(v.free).plus(v.locked).isZero())
+    .map((v) => {
+      const bridgedPrice = resolveTokenPrice(v.asset);
+
+      if (bridgedPrice) {
+        return {
+          symbol: v.asset,
+          amount: new BN(v.free).plus(v.locked).multipliedBy(bridgedPrice).toString(10),
+        };
+      }
+
+      return {
+        symbol: v.asset, // stablecoin
+        amount: new BN(v.free).plus(v.locked).multipliedBy(1).toString(10),
+      };
+    });
+
+  console.warn(assetsOnBalance);
 
   return process.done();
 };
