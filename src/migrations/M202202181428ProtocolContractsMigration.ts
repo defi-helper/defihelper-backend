8225c7bc0a808a851bfae7e7ca83ba2be827c336
Luka Nikitin <lukentuichennel@gmail.com>
2022-02-21 14:53:06 +0300

    wip

--- src/migrations/M202202181428ProtocolContractsMigration.ts
+++ src/migrations/M202202181428ProtocolContractsMigration.ts
@@ -1,21 +1,36 @@
 import { SchemaBuilder } from 'knex';
 import { walletTableName } from '@models/Wallet/Entity';
 import container from '@container';
+import { contractBlockchainTableName, contractDebankTableName } from '@models/Protocol/Entity';
+import { contractTableName } from '@models/Automate/Entity';
 
 export default async (schema: SchemaBuilder) => {
   const database = container.database();
 
-  // await database.raw(`
-  //   insert into protocol_contract_backend (id, type, blockchain, network, "publicKey", address)
-  //     select
-  //         rootWallet.id, rootWallet.type, rootWallet.blockchain, rootWallet.network, rootWallet."publicKey", rootWallet.address
-  //     from wallet AS rootWallet
-  // `);
-  // return schema.alterTable(walletTableName, (table) => {
-  //   table.dropColumn('type');
-  //   table.dropColumn('blockchain');
-  //   table.dropColumn('network');
-  //   table.dropColumn('publicKey');
-  //   table.dropColumn('address');
-  // });
+  await database.raw(`
+    insert into ${contractBlockchainTableName} (id, blockchain, network, address, metric, automate, "deployBlockNumber", layout, adapter)
+      select
+          rootContract.id, rootContract.blockchain, rootContract.network, rootContract.address, rootContract.metric, rootContract.automate, rootContract."deployBlockNumber", rootContract.layout, rootContract.adapter
+      from protocol_contract AS rootContract where adapter != 'debankApiReadonly'
+  `);
+
+  await database.raw(`
+    insert into ${contractDebankTableName} (id, address, metric)
+      select
+          rootContract.id, rootContract."debankAddress", rootContract.metric
+      from protocol_contract AS rootContract where adapter = 'debankApiReadonly'
+    on conflict do nothing
+  `);
+
+  return schema.alterTable(contractTableName, (table) => {
+    table.dropColumn('debankAddress');
+    table.dropColumn('blockchain');
+    table.dropColumn('network');
+    table.dropColumn('metric');
+    table.dropColumn('address');
+    table.dropColumn('automate');
+    table.dropColumn('deployBlockNumber');
+    table.dropColumn('layout');
+    table.dropColumn('adapter');
+  });
 };
