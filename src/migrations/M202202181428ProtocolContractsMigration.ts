3d83aee8866bfdae4e031cddc316a7e3d7580044
Luka Nikitin <lukentuichennel@gmail.com>
2022-02-21 15:32:09 +0300

    fix

--- src/migrations/M202202181428ProtocolContractsMigration.ts
+++ src/migrations/M202202181428ProtocolContractsMigration.ts
@@ -1,17 +1,20 @@
 import { SchemaBuilder } from 'knex';
-import { walletTableName } from '@models/Wallet/Entity';
 import container from '@container';
-import { contractBlockchainTableName, contractDebankTableName } from '@models/Protocol/Entity';
-import { contractTableName } from '@models/Automate/Entity';
+import {
+  contractBlockchainTableName,
+  contractDebankTableName,
+  contractTableName,
+} from '@models/Protocol/Entity';
 
 export default async (schema: SchemaBuilder) => {
   const database = container.database();
 
   await database.raw(`
-    insert into ${contractBlockchainTableName} (id, blockchain, network, address, metric, automate, "deployBlockNumber", layout, adapter)
+    insert into ${contractBlockchainTableName} (id, blockchain, network, address, metric, automate, "deployBlockNumber", adapter)
       select
-          rootContract.id, rootContract.blockchain, rootContract.network, rootContract.address, rootContract.metric, rootContract.automate, rootContract."deployBlockNumber", rootContract.layout, rootContract.adapter
+          rootContract.id, rootContract.blockchain, rootContract.network, rootContract.address, rootContract.metric, rootContract.automate, rootContract."deployBlockNumber", rootContract.adapter
       from protocol_contract AS rootContract where adapter != 'debankApiReadonly'
+    on conflict do nothing
   `);
 
   await database.raw(`
@@ -30,7 +33,6 @@ export default async (schema: SchemaBuilder) => {
     table.dropColumn('address');
     table.dropColumn('automate');
     table.dropColumn('deployBlockNumber');
-    table.dropColumn('layout');
     table.dropColumn('adapter');
   });
 };
