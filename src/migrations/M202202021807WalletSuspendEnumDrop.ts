1648d7dff18cdad83efeb892aeb6cfcf2780b5b0
Bashka <artur-mamedbekov@yandex.ru>
2022-03-04 10:00:23 +0300

    fix

--- src/migrations/M202202021807WalletSuspendEnumDrop.ts
+++ src/migrations/M202202021807WalletSuspendEnumDrop.ts
@@ -1,5 +1,5 @@
 import { SchemaBuilder } from 'knex';
 
 export default async (schema: SchemaBuilder) => {
-  return schema.raw('ALTER TABLE wallet DROP CONSTRAINT "wallet_suspendReason_check";');
+  return schema.raw('ALTER TABLE wallet DROP CONSTRAINT IF EXISTS "wallet_suspendReason_check";');
 };
