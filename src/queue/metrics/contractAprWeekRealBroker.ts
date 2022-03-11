5ca7a93e24b43233608d4bb4be962d4bcbacf39b
Bashka <artur-mamedbekov@yandex.ru>
2022-03-11 17:50:19 +0300

    enable real apy for all contracts

--- src/queue/metrics/contractAprWeekRealBroker.ts
+++ src/queue/metrics/contractAprWeekRealBroker.ts
@@ -3,10 +3,7 @@ import { Process } from '@models/Queue/Entity';
 
 export default async (process: Process) => {
   const queue = container.model.queueService();
-  const contracts = await container.model
-    .contractTable()
-    .where('hidden', false)
-    .andWhere('protocol', 'c099eeec-1a60-4b62-98e0-2fbc4c958699'); // for test
+  const contracts = await container.model.contractTable().where('hidden', false);
   await Promise.all(
     contracts.map((contract) =>
       queue.push(
