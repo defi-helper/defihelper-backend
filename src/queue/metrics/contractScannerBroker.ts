65fe3fe4b08ebe2fb54cbcf51d59e68f8cbcd057
Bashka <artur-mamedbekov@yandex.ru>
2022-03-15 11:37:29 +0300

    blockchain contract selector fix for uniq wallets broker

--- src/queue/metrics/contractScannerBroker.ts
+++ src/queue/metrics/contractScannerBroker.ts
@@ -1,9 +1,17 @@
 import container from '@container';
+import { contractBlockchainTableName, contractTableName } from '@models/Protocol/Entity';
 import { Process } from '@models/Queue/Entity';
 
 export default async (process: Process) => {
   const queue = container.model.queueService();
-  const contracts = await container.model.contractTable().where('blockchain', 'ethereum');
+  const contracts = await container.model
+    .contractTable()
+    .innerJoin(
+      contractBlockchainTableName,
+      `${contractBlockchainTableName}.id`,
+      `${contractTableName}.id`,
+    )
+    .where(`${contractBlockchainTableName}.blockchain`, 'ethereum');
   await Promise.all(
     contracts.map((contract) =>
       queue.push('metricsContractScannerDate', { contract: contract.id }),
