90ecfde671de9f87d931465594a39c60d86e997d
Luka Nikitin <lukentuichennel@gmail.com>
2022-03-16 19:24:01 +0200

    fix

--- src/api/schema/landing/index.ts
+++ src/api/schema/landing/index.ts
@@ -87,6 +87,13 @@ export const LandingMediumPostsQuery: GraphQLFieldConfig<any, Request> = {
         .then((rows) =>
           rows.map((v) => ({
             ...v,
+            text: v.text
+              .replace(/(<([^>]+)>)/gi, '')
+              .slice(0, 300)
+              .split(' ')
+              .slice(0, -1)
+              .concat('...')
+              .join(' '),
             createdAt: dayjs.unix(v.createdAt),
           })),
         );
