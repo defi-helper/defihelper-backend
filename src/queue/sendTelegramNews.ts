d9dc199dc94aba4aebdcd6739e69fd38f269fa88
Luka Nikitin <lukentuichennel@gmail.com>
2022-04-08 14:19:27 +0200

    fix

--- src/queue/sendTelegramNews.ts
+++ src/queue/sendTelegramNews.ts
@@ -1,12 +1,15 @@
 import container from '@container';
-import { ContactBroker, ContactStatus } from '@models/Notification/Entity';
+import { ContactBroker, ContactStatus, userContactTableName } from '@models/Notification/Entity';
 import { Process } from '@models/Queue/Entity';
 
 export default async (process: Process) => {
-  const contacts = await container.model.userContactTable().where({
-    status: ContactStatus.Active,
-    broker: ContactBroker.Telegram,
-  });
+  const contacts = await container.model
+    .userContactTable()
+    .where({
+      status: ContactStatus.Active,
+      broker: ContactBroker.Telegram,
+    })
+    .groupByRaw(`${userContactTableName}.params->>'chatId', ${userContactTableName}.id`);
 
   await Promise.all(
     contacts.map((contact) => {
