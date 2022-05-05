78f831956130a8f8ad19f22ba67377a75b888143
artur <amamedbekov@tokenomica.com>
2022-05-05 14:17:36 +0300

    get metrics only for blockchain contracts

--- src/queue/metrics/contractBroker.ts
+++ src/queue/metrics/contractBroker.ts
@@ -1,9 +1,13 @@
 import container from '@container';
+import { contractBlockchainTableName, contractTableName } from '@models/Protocol/Entity';
 import { Process } from '@models/Queue/Entity';
 
 export default async (process: Process) => {
   const queue = container.model.queueService();
-  const contracts = await container.model.contractTable();
+  const contracts = await container.model
+    .contractTable()
+    .innerJoin(contractBlockchainTableName, `${contractTableName}.id`, `${contractTableName}.id`)
+    .andWhere(`${contractTableName}.deprecated`, false);
   await Promise.all(
     contracts.map((contract) =>
       queue.push('metricsContractCurrent', { contract: contract.id }, { topic: 'metricCurrent' }),
