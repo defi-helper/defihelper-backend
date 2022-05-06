ad321e040a3343d96bc1ac93f890b69bafe909be
artur <amamedbekov@tokenomica.com>
2022-05-06 12:00:17 +0300

    get wallet metrics only for not deprecated contract

--- src/queue/metrics/walletBroker.ts
+++ src/queue/metrics/walletBroker.ts
@@ -1,9 +1,17 @@
 import container from '@container';
+import { contractTableName, walletContractLinkTableName } from '@models/Protocol/Entity';
 import { Process } from '@models/Queue/Entity';
 
 export default async (process: Process) => {
   const queue = container.model.queueService();
-  const links = await container.model.walletContractLinkTable();
+  const links = await container.model
+    .walletContractLinkTable()
+    .innerJoin(
+      contractTableName,
+      `${contractTableName}.id`,
+      `${walletContractLinkTableName}.contract`,
+    )
+    .andWhere(`${contractTableName}.deprecated`, false);
   await Promise.all(
     links.map(async (link) => {
       queue.push(
