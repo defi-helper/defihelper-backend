11546d741b0449a1d56b880087caa61d38b571c0
Luka Nikitin <lukentuichennel@gmail.com>
2022-05-20 15:50:01 +0200

    new networks

--- src/services/Blockchain/Ethereum.ts
+++ src/services/Blockchain/Ethereum.ts
@@ -221,7 +221,6 @@ export interface Config {
   moonriver: NetworkConfig;
   moonbeam: NetworkConfig;
   avalanche: NetworkConfig;
-  harmony: NetworkConfig;
   avalancheTestnet: NetworkConfig;
   local: NetworkConfig;
 }
