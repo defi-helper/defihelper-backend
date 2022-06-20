d2c483888d6fb565c2f4df6a30ed51f910c8fd25
Luka Nikitin <lukentuichennel@gmail.com>
2022-06-20 10:57:33 +0200

    fix user column

--- src/queue/migrateNotifications.ts
+++ src/queue/migrateNotifications.ts
@@ -2,7 +2,7 @@ import container from '@container';
 import { Process } from '@models/Queue/Entity';
 
 export default async (process: Process) => {
-  const notifications = await container.model.userNotificationTable().whereNot('user', null);
+  const notifications = await container.model.userNotificationTable();
   const contacts = await container.model.userContactTable().whereIn(
     'user',
     notifications.map((v: any) => v.user),
@@ -10,6 +10,10 @@ export default async (process: Process) => {
 
   await Promise.all(
     notifications.map(async (notification) => {
+      if (!(notification as any).user) {
+        return null;
+      }
+
       const contact = contacts.find((v) => v.id === (notification as any).user);
       if (!contact) return null;
 
