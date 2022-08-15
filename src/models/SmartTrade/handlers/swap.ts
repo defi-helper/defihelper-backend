fe57797e361a0386a51d3084a559b491b14ce12c
artur <amamedbekov@tokenomica.com>
2022-08-15 16:16:11 +0300

    debug messages for smart trade handler

--- src/models/SmartTrade/handlers/swap.ts
+++ src/models/SmartTrade/handlers/swap.ts
@@ -2,6 +2,7 @@ import container from '@container';
 import { isKey } from '@services/types';
 import contracts from '@defihelper/networks/contracts.json';
 import { useEthereumFreeConsumer } from '@services/Blockchain/Consumer';
+import { LogJsonMessage } from '@services/Log';
 import { ethers } from 'ethers';
 import { BigNumber as BN } from 'bignumber.js';
 import { Order, SwapCallData } from '../Entity';
@@ -11,6 +12,9 @@ import SmartTradeRouterABI from '../data/smartTradeRouterABI.json';
 export default async function (
   order: Order<SwapCallData>,
 ): Promise<ethers.ContractTransaction | Error | null> {
+  const log = LogJsonMessage.debug({ source: 'smartTradeSwapHandler' });
+  log.ex({ orderId: order.id }).send();
+
   if (!isKey(contracts, order.network)) {
     return new Error('Contracts not deployed to target network');
   }
@@ -21,6 +25,12 @@ export default async function (
   const actualAmountOut = await uniswapRouter
     .getAmountsOut(order.callData.amountIn, order.callData.path)
     .then((amountsOut: ethers.BigNumber[]) => new BN(amountsOut[amountsOut.length - 1].toString()));
+  log
+    .ex({
+      expectedAmountOut: order.callData.amountOut,
+      actualAmountOut: actualAmountOut.toString(10),
+    })
+    .send();
   if (
     (order.callData.direction === 'gt' && actualAmountOut.lt(order.callData.amountOut)) ||
     (order.callData.direction === 'lt' && actualAmountOut.gt(order.callData.amountOut))
@@ -32,6 +42,7 @@ export default async function (
   }
 
   const freeConsumer = await useEthereumFreeConsumer(order.network);
+  log.ex({ consumer: freeConsumer?.consumer.address }).send();
   if (freeConsumer === null) return new Error('Not free consumer');
 
   const networkContracts = contracts[order.network] as { [name: string]: { address: string } };
@@ -48,6 +59,7 @@ export default async function (
     const gasLimit = new BN(estimateGas).multipliedBy(1.1).toFixed(0);
     const gasPrice = await provider.getGasPrice().then((v) => v.toString());
     const gasFee = new BN(gasLimit).multipliedBy(gasPrice).toFixed(0);
+    log.ex({ estimateGas, gasLimit, gasPrice, gasFee }).send();
     return smartTradeRouter.handleOrder(order.number, gasFee, {
       gasLimit,
       gasPrice,
