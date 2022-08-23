1e42f011c3f83f10c8802a7e1adac555e4c0c955
Luka Nikitin <lukentuichennel@gmail.com>
2022-08-23 16:21:46 +0200

    wip

--- src/queue/contract/uniswapRouteRevealerBroker.ts
+++ src/queue/contract/uniswapRouteRevealerBroker.ts
@@ -1,5 +1,6 @@
 import { Process } from '@models/Queue/Entity';
 import container from '@container';
+import dayjs from 'dayjs';
 
 export interface Params {
   id: string;
@@ -8,7 +9,20 @@ export interface Params {
 export default async (process: Process) => {
   const uniswapRoutableTokens = await container.model.tokenTable().where('priceFeedNeeded', true);
 
-  
+  const lag = 86400 / uniswapRoutableTokens.length;
+  await uniswapRoutableTokens.reduce<Promise<dayjs.Dayjs>>(async (prev, token) => {
+    const startAt = await prev;
+
+    await container.model.queueService().push(
+      'tokenResolveUniswapRoute',
+      {
+        id: token.id,
+      },
+      { startAt: startAt.toDate() },
+    );
+
+    return startAt.clone().add(lag, 'seconds');
+  }, Promise.resolve(dayjs()));
 
   return process.done();
 };
