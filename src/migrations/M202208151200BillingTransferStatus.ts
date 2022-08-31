089f5f0633995cc11a070b53939747b768587d48
artur <amamedbekov@tokenomica.com>
2022-08-31 10:44:59 +0300

    two migration for update transfer billing schema

--- src/migrations/M202208151200BillingTransferStatus.ts
+++ src/migrations/M202208151200BillingTransferStatus.ts
@@ -3,24 +3,13 @@ import { TransferStatus, transferTableName } from '@models/Billing/Entity';
 import container from '@container';
 
 export default async (schema: SchemaBuilder) => {
-  console.log('migration start');
-
   await schema.alterTable(transferTableName, (table) => {
     table.string('status', 36).notNullable().index().defaultTo(TransferStatus.Pending);
     table.text('rejectReason').notNullable().defaultTo('');
   });
-  console.log('migration alterTable complete');
 
   await container.model
     .billingTransferTable()
     .update('status', TransferStatus.Confirmed)
     .where('confirmed', true);
-
-  console.log('migration update');
-
-  await schema.alterTable(transferTableName, (table) => {
-    table.dropColumn('confirmed');
-  });
-
-  console.log('migration alterTable drop');
 };
