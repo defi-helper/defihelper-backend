487be62ea8c4fa22042625196724c49f312797c0
Luka Nikitin <lukentuichennel@gmail.com>
2022-09-07 17:20:44 +0200

    done

--- src/queue/metrics/walletBalances/walletProtocolsBalancesDeBankBroker.ts
+++ src/queue/metrics/walletBalances/walletProtocolsBalancesDeBankBroker.ts
@@ -6,24 +6,42 @@ import {
   walletTableName,
   WalletBlockchainType,
 } from '@models/Wallet/Entity';
+import { tableName as userTableName } from '@models/User/Entity';
+import BN from 'bignumber.js';
 
 export default async (process: Process) => {
   const wallets = await container.model
     .walletTable()
     .distinctOn(`${walletBlockchainTableName}.address`)
     .column(`${walletBlockchainTableName}.id`)
+    .column(`${walletTableName}.id as userId`)
+    .column(`${userTableName}.createdAt as registeredAt`)
     .innerJoin(
       walletBlockchainTableName,
       `${walletBlockchainTableName}.id`,
       `${walletTableName}.id`,
     )
+    .innerJoin(userTableName, `${walletTableName}.user`, `${userTableName}.id`)
     .where(`${walletBlockchainTableName}.type`, WalletBlockchainType.Wallet)
     .andWhere(`${walletBlockchainTableName}.blockchain`, 'ethereum')
     .whereNull(`${walletTableName}.deletedAt`);
 
+  const contactsCount = await container.model
+    .userContactTable()
+    .column('user')
+    .count('id')
+    .groupBy('user')
+    .then((rows) => new Map(rows.map((row) => [row.user, row.count])));
+
   const lag = 600 / wallets.length;
-  await wallets.reduce<Promise<dayjs.Dayjs>>(async (prev, { id }) => {
+  await wallets.reduce<Promise<dayjs.Dayjs>>(async (prev, { id, userId, registeredAt }) => {
     const startAt = await prev;
+
+    const contractsCount = new BN(contactsCount.get(userId) ?? 0);
+
+    if (contractsCount.isZero() && dayjs().diff(dayjs(registeredAt), 'day', true) > 30) {
+      return startAt;
+    }
 
     await container.model
       .queueService()
