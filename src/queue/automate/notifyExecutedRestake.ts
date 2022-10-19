0e0186725b41a670842cce18c22f30ce1a926c2d
Luka Nikitin <lukentuichennel@gmail.com>
2022-10-19 16:10:52 +0300

    wip no verify

--- src/queue/automate/notifyExecutedRestake.ts
+++ src/queue/automate/notifyExecutedRestake.ts
@@ -1,10 +1,5 @@
 import container from '@container';
 import { Process } from '@models/Queue/Entity';
-import {
-  walletBlockchainTableName,
-  walletExchangeTableName,
-  walletTableName,
-} from '@models/Wallet/Entity';
 
 export interface Params {
   contract: string;
@@ -13,12 +8,8 @@ export interface Params {
 export default async (process: Process) => {
   const { contract } = process.task.params as Params;
 
-  const [contract, walletsExchange] = await Promise.all([
+  const [contractaa] = await Promise.all([
     container.model.automateContractTable().where('id', contract).first(),
-    container.model
-      .walletTable()
-      .innerJoin(walletExchangeTableName, `${walletExchangeTableName}.id`, `${walletTableName}.id`)
-      .where(`${walletTableName}.user`, user),
   ]);
 
   return process.done();
